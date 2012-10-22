ce8596a5baaa29e194d05a6a7e2a8de721b95cda
Ednara Oliveira <ednaraoliveira@gmail.com>
2012-10-22 14:33:30 -0300

    Refatoração de testes unitários

--- impl/core/src/test/java/br/gov/frameworkdemoiselle/internal/configuration/SecurityConfigTest.java
+++ impl/core/src/test/java/br/gov/frameworkdemoiselle/internal/configuration/SecurityConfigTest.java
@@ -5,7 +5,9 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
-
+import br.gov.frameworkdemoiselle.security.Authenticator;
+import br.gov.frameworkdemoiselle.security.Authorizer;
+import br.gov.frameworkdemoiselle.security.User;
 
 public class SecurityConfigTest {
 
@@ -21,4 +23,58 @@ public class SecurityConfigTest {
 		assertEquals(true, config.isEnabled());
 	}
 
+	@Test
+	public void testSetEnabled() {
+		config.setEnabled(false);
+		assertEquals(false, config.isEnabled());
+	}
+
+	@Test
+	public void testSetAuthenticatorClass() {
+		Authenticator authenticator = new TestAuthenticator();
+		config.setAuthenticatorClass(authenticator.getClass());
+		assertEquals("br.gov.frameworkdemoiselle.internal.configuration.TestAuthenticator", config
+				.getAuthenticatorClass().getName());
+	}
+	
+	@Test
+	public void testSetAuthorizerClass() {
+		Authorizer authorizer = new TestAuthorizer();
+		config.setAuthorizerClass(authorizer.getClass());
+		assertEquals("br.gov.frameworkdemoiselle.internal.configuration.TestAuthorizer", config
+				.getAuthorizerClass().getName());
+	}
+
 }
+
+class TestAuthenticator implements Authenticator {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public boolean authenticate() {
+		return false;
+	}
+
+	@Override
+	public void unAuthenticate() {
+	}
+
+	@Override
+	public User getUser() {
+		return null;
+	}
+}
+
+class TestAuthorizer implements Authorizer{
+
+	@Override
+	public boolean hasRole(String role) {
+		return false;
+	}
+
+	@Override
+	public boolean hasPermission(String resource, String operation) {
+		return false;
+	}
+} 
