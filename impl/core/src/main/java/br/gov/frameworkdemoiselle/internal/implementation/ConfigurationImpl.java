197d7321ff14e075daa48d43f106ea196044e72b
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2013-03-12 15:59:35 -0300

    Serialização das classes Conf

--- impl/core/src/main/java/br/gov/frameworkdemoiselle/internal/implementation/ConfigurationImpl.java
+++ impl/core/src/main/java/br/gov/frameworkdemoiselle/internal/implementation/ConfigurationImpl.java
@@ -1,9 +1,13 @@
 package br.gov.frameworkdemoiselle.internal.implementation;
 
+import java.io.Serializable;
+
 import br.gov.frameworkdemoiselle.internal.configuration.ConfigurationLoader;
 import br.gov.frameworkdemoiselle.util.Beans;
 
-public class ConfigurationImpl {
+public class ConfigurationImpl implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	private boolean loaded = false;
 
