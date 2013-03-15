e386cfd7fc123ba7d564594c7f3d6c6f3230e34a
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2013-03-15 14:33:11 -0300

    Correção no erro de injeção após as modificações

--- impl/core/src/main/java/br/gov/frameworkdemoiselle/template/DelegateCrud.java
+++ impl/core/src/main/java/br/gov/frameworkdemoiselle/template/DelegateCrud.java
@@ -41,6 +41,7 @@ import java.util.ListIterator;
 
 import br.gov.frameworkdemoiselle.internal.implementation.DefaultTransaction;
 import br.gov.frameworkdemoiselle.transaction.Transaction;
+import br.gov.frameworkdemoiselle.transaction.TransactionContext;
 import br.gov.frameworkdemoiselle.transaction.Transactional;
 import br.gov.frameworkdemoiselle.util.Beans;
 import br.gov.frameworkdemoiselle.util.Reflections;
@@ -142,6 +143,8 @@ public class DelegateCrud<T, I, C extends Crud<T, I>> implements Crud<T, I> {
 		} else {
 			nonTransactionalInsert(bean);
 		}
+		
+		System.out.println();
 	}
 
 	@Transactional
@@ -188,6 +191,7 @@ public class DelegateCrud<T, I, C extends Crud<T, I>> implements Crud<T, I> {
 	}
 
 	private boolean isRunningTransactionalOperations() {
-		return !(Beans.getReference(Transaction.class) instanceof DefaultTransaction);
+		Transaction transaction = Beans.getReference(TransactionContext.class).getCurrentTransaction();
+		return !(transaction instanceof DefaultTransaction);
 	}
 }
--- impl/core/src/main/java/br/gov/frameworkdemoiselle/transaction/TransactionalInterceptor.java
+++ impl/core/src/main/java/br/gov/frameworkdemoiselle/transaction/TransactionalInterceptor.java
@@ -39,7 +39,6 @@ package br.gov.frameworkdemoiselle.transaction;
 import java.io.Serializable;
 
 import javax.enterprise.context.ContextNotActiveException;
-import javax.enterprise.context.RequestScoped;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
@@ -196,44 +195,5 @@ public class TransactionalInterceptor implements Serializable {
 		}
 
 		return logger;
-	}
-
-	@RequestScoped
-	class TransactionInfo implements Serializable {
-
-		private static final long serialVersionUID = 1L;
-
-		private int counter = 0;
-
-		private boolean owner;
-
-		public TransactionInfo() {
-			clear();
-		}
-
-		public void clear() {
-			this.owner = false;
-			this.counter = 0;
-		}
-
-		public int getCounter() {
-			return counter;
-		}
-
-		public void incrementCounter() {
-			this.counter++;
-		}
-
-		public void decrementCounter() {
-			this.counter--;
-		}
-
-		public void markAsOwner() {
-			this.owner = true;
-		}
-
-		public boolean isOwner() {
-			return owner;
-		}
 	}
 }
