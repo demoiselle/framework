4d02f140be0997cead866194513772d7efcbd18e
Emerson Oliveira <emerson.sdoliveira@gmail.com>
2013-03-27 16:14:05 -0300

    Adicionados testes para classe de configuração com prefixo

---
+++ impl/core/src/test/java/br/gov/frameworkdemoiselle/configuration/prefix/MissingPrefix.java
@@ -0,0 +1,7 @@
+package br.gov.frameworkdemoiselle.configuration.prefix;
+
+import br.gov.frameworkdemoiselle.configuration.Configuration;
+
+@Configuration(prefix="missing.prefix.")
+public class MissingPrefix extends AbstractPrefixConfig{
+}
