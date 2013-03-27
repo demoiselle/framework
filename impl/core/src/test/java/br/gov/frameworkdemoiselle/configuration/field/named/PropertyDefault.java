e0bcac42ddd2de81a3ed0ba0769592ea993e539f
Emerson Oliveira <emerson.sdoliveira@gmail.com>
2013-03-27 10:23:13 -0300

    Adicionados alguns testes para campos anotados com @Name

---
+++ impl/core/src/test/java/br/gov/frameworkdemoiselle/configuration/field/named/PropertyDefault.java
@@ -0,0 +1,7 @@
+package br.gov.frameworkdemoiselle.configuration.field.named;
+
+import br.gov.frameworkdemoiselle.configuration.Configuration;
+
+@Configuration
+public class PropertyDefault extends AbstractNamedFieldConfig {
+}
