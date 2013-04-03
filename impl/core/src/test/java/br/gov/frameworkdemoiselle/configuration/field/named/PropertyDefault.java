092d105908c7ce7fa5a5d595647d763ad2bb71c6
Emerson Oliveira <emerson.sdoliveira@gmail.com>
2013-04-03 17:28:23 -0300

    Acrescentados testes com carregamento de configurações a partir de arquivo xml

--- impl/core/src/test/java/br/gov/frameworkdemoiselle/configuration/field/named/PropertyDefault.java
+++
@@ -1,7 +0,0 @@
-package br.gov.frameworkdemoiselle.configuration.field.named;
-
-import br.gov.frameworkdemoiselle.configuration.Configuration;
-
-@Configuration
-public class PropertyDefault extends AbstractNamedFieldConfig {
-}
