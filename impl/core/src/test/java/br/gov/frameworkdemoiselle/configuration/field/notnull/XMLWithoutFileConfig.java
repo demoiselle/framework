340200a3f465edc4de25a418c0c160ec2593f0e1
Emerson Oliveira <emerson.sdoliveira@gmail.com>
2013-04-04 14:13:47 -0300

    Incremento dos testes com atributos anotados com @NotNull, adicionando propriedades sendo carregadas a partir de arquivo xml e refatoração com modificação do nome da variável anotada

---
+++ impl/core/src/test/java/br/gov/frameworkdemoiselle/configuration/field/notnull/XMLWithoutFileConfig.java
@@ -0,0 +1,8 @@
+package br.gov.frameworkdemoiselle.configuration.field.notnull;
+
+import static br.gov.frameworkdemoiselle.configuration.ConfigType.XML;
+import br.gov.frameworkdemoiselle.configuration.Configuration;
+
+@Configuration(resource = "nofile", type = XML)
+public class XMLWithoutFileConfig extends AbstractNotNullFieldConfig {
+}
---
+++ impl/core/src/test/java/br/gov/frameworkdemoiselle/configuration/field/notnull/XMLWithoutNotNullField.java
@@ -0,0 +1,8 @@
+package br.gov.frameworkdemoiselle.configuration.field.notnull;
+
+import static br.gov.frameworkdemoiselle.configuration.ConfigType.XML;
+import br.gov.frameworkdemoiselle.configuration.Configuration;
+
+@Configuration(resource = "without-field", type = XML)
+public class XMLWithoutNotNullField extends AbstractNotNullFieldConfig {
+}
