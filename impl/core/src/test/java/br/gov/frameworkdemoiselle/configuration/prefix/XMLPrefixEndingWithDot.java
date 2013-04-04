13bc29d587b455bfc34eed86ffa93999ff27747a
Emerson Oliveira <emerson.sdoliveira@gmail.com>
2013-04-04 16:07:13 -0300

    Acrescentados testes de carregamento a partir de arquivo xml nos testes de prefixo

---
+++ impl/core/src/test/java/br/gov/frameworkdemoiselle/configuration/prefix/XMLPrefixEndingWithDot.java
@@ -0,0 +1,8 @@
+package br.gov.frameworkdemoiselle.configuration.prefix;
+
+import static br.gov.frameworkdemoiselle.configuration.ConfigType.XML;
+import br.gov.frameworkdemoiselle.configuration.Configuration;
+
+@Configuration(type = XML, prefix = "endingWithDot.")
+public class XMLPrefixEndingWithDot extends AbstractPrefixConfig {
+}
