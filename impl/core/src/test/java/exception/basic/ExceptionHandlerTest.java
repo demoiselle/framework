ec4dd5d247898391187b3bbd698f2f3426898cfc
Dancovich <danilo.viana@serpro.gov.br>
2013-05-06 10:54:35 -0300

    Inserida notificação de licença LGPL nas classes do módulo de monitoração e gerenciamento.

--- impl/core/src/test/java/exception/basic/ExceptionHandlerTest.java
+++ impl/core/src/test/java/exception/basic/ExceptionHandlerTest.java
@@ -41,8 +41,6 @@ import static junit.framework.Assert.fail;
 
 import javax.inject.Inject;
 
-import junit.framework.Assert;
-
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
