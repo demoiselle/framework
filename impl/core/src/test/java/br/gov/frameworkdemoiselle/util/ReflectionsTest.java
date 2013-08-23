d78978afce4d09372d4eb43a0935f6a7fa7576ce
vfcosta <vfcosta@gmail.com>
2013-08-23 09:40:26 -0300

    Ajuste para considerar uma hierarquia de qualquer tamanho para classes que herdam de DelegateCrud

--- impl/core/src/test/java/br/gov/frameworkdemoiselle/util/ReflectionsTest.java
+++ impl/core/src/test/java/br/gov/frameworkdemoiselle/util/ReflectionsTest.java
@@ -56,6 +56,12 @@ public class ReflectionsTest {
 		assertEquals(Long.class, Reflections.getGenericTypeArgument(members[0], 0));
 		assertEquals(String.class, Reflections.getGenericTypeArgument(members[1], 0));
 	}
+
+	@Test
+	public void testGetGenericTypeArgumentClassMultipleChildren() {
+		assertEquals(Long.class, Reflections.getGenericTypeArgument(OtherClass3.class, 0));
+		assertEquals(String.class, Reflections.getGenericTypeArgument(OtherClass3.class, 1));
+	}
 }
 
 class SomeClass<T, I> {
@@ -70,4 +76,12 @@ class OtherClass extends SomeClass<Long, String> {
 	public Class<Long> number;
 
 	public Class<String> text;
+}
+
+class OtherClass2 extends OtherClass {
+
+}
+
+class OtherClass3 extends OtherClass2 {
+
 }
