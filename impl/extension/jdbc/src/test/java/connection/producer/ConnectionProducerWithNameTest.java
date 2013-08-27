17c4efee524211236cb0e39e0d181fe3f962c4ee
Dancovich <danilo.viana@serpro.gov.br>
2013-08-27 15:49:08 -0300

    Removidos imports desnecessários

--- impl/extension/jdbc/src/test/java/connection/producer/ConnectionProducerWithNameTest.java
+++ impl/extension/jdbc/src/test/java/connection/producer/ConnectionProducerWithNameTest.java
@@ -45,7 +45,6 @@ import javax.inject.Inject;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
