f2be871b55686345e813325438bc97dea29a3aeb
lusabo <lucianosantosborges@gmail.com>
2013-09-18 17:03:04 -0300

    IN PROGRESS - issue FWK-119: Testes da extensão Servlet https://demoiselle.atlassian.net/browse/FWK-119

--- impl/extension/servlet/src/test/java/security/SecurityTest.java
+++ impl/extension/servlet/src/test/java/security/SecurityTest.java
@@ -1,10 +1,14 @@
-package xxxx;
+package security;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import java.io.IOException;
 import java.net.URL;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -16,43 +20,31 @@ import org.junit.runner.RunWith;
 import test.Tests;
 
 @RunWith(Arquillian.class)
-public class XTest {
+public class SecurityTest {
 
-	private static final String PATH = "src/test/resources/xxx";
+	private static final String PATH = "src/test/resources/security";
 
 	@ArquillianResource
 	private URL deploymentUrl;
 
 	@Deployment(testable = false)
 	public static WebArchive createDeployment() {
-		return Tests.createDeployment().addClass(XServlet.class)
+		return Tests.createDeployment().addClass(SecurityServlet.class)
 				.addAsWebInfResource(Tests.createFileAsset(PATH + "/web.xml"), "web.xml");
 	}
 
 	@Test
-	public void xxxx() {
+	public void login() {
 		HttpClient client = new HttpClient();
 		GetMethod method = new GetMethod(deploymentUrl + "/login");
-
 		try {
 			int status = client.executeMethod(method);
-			System.out.println(status);
-
+			assertEquals(HttpStatus.SC_OK, status);
 		} catch (HttpException e) {
-			e.printStackTrace();
-
+			fail();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
-		// browser.open(deploymentUrl + "login");
-
-		// browser.type("id=xxx-input", "demo");
-		// browser.waitForPageToLoad("15000");
-
-		// assertTrue("User should be logged in!",
-		// browser.isElementPresent("xpath=//li[contains(text(), 'Welcome')]"));
-		// assertTrue("Username should be shown!",
-		// browser.isElementPresent("xpath=//p[contains(text(), 'You are signed in as demo.')]"));
 	}
+
 }
