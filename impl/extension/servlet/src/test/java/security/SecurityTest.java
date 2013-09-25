ceb0887ed442006d52440d9f6fc9d7d183847178
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2013-09-25 13:24:24 -0300

    IN PROGRESS - issue FWK-119: Testes da extensão Servlet https://demoiselle.atlassian.net/browse/FWK-119

--- impl/extension/servlet/src/test/java/security/SecurityTest.java
+++ impl/extension/servlet/src/test/java/security/SecurityTest.java
@@ -3,15 +3,15 @@ package security;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.HttpParams;
-import org.apache.http.params.HttpParamsNames;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
@@ -38,24 +38,24 @@ public class SecurityTest {
 	}
 
 	@Test
-	public void loginSucessfull() throws ClientProtocolException, IOException {
-		DefaultHttpClient client = new DefaultHttpClient();
-//		HttpGet get = new HttpGet(deploymentUrl + "/login");
-//		get.getParams().setParameter("username", "demoiselle");
-//		get.getParams().setParameter("password", "changeit");
-		HttpGet get = new HttpGet(deploymentUrl + "/login?username=demoiselle&password=changeit");
-		HttpResponse response = client.execute(get);
+	public void loginSucessfull() throws ClientProtocolException, IOException, URISyntaxException {
+		URIBuilder uriBuilder = new URIBuilder(deploymentUrl + "/login");
+		uriBuilder.setParameter("username", "demoiselle");
+		uriBuilder.setParameter("password", "changeit");
+
+		HttpGet httpGet = new HttpGet(uriBuilder.build());
+		HttpResponse response = new DefaultHttpClient().execute(httpGet);
 
 		int status = response.getStatusLine().getStatusCode();
 		assertEquals(HttpStatus.SC_OK, status);
 	}
-	
+
 	@Test
 	public void loginFailed() throws ClientProtocolException, IOException {
 		DefaultHttpClient client = new DefaultHttpClient();
 		HttpGet get = new HttpGet(deploymentUrl + "/login?username=demoiselle&password=wrongpass");
 		HttpResponse response = client.execute(get);
-		
+
 		int status = response.getStatusLine().getStatusCode();
 		assertEquals(HttpStatus.SC_UNAUTHORIZED, status);
 	}
