d2ac773fbe93461e1e44cb4d060b602bf495a05e
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2013-09-25 15:39:29 -0300

    IN PROGRESS - issue FWK-119: Testes da extensão Servlet https://demoiselle.atlassian.net/browse/FWK-119

--- impl/extension/servlet/src/test/java/security/SecurityTest.java
+++ impl/extension/servlet/src/test/java/security/SecurityTest.java
@@ -1,5 +1,7 @@
 package security;
 
+import static org.apache.http.HttpStatus.SC_FORBIDDEN;
+import static org.apache.http.HttpStatus.SC_OK;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
@@ -7,11 +9,10 @@ import java.net.URISyntaxException;
 import java.net.URL;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
@@ -44,19 +45,22 @@ public class SecurityTest {
 		uriBuilder.setParameter("password", "changeit");
 
 		HttpGet httpGet = new HttpGet(uriBuilder.build());
-		HttpResponse httpResponse = new DefaultHttpClient().execute(httpGet);
+		HttpResponse httpResponse = HttpClientBuilder.create().build().execute(httpGet);
 
 		int status = httpResponse.getStatusLine().getStatusCode();
-		assertEquals(HttpStatus.SC_OK, status);
+		assertEquals(SC_OK, status);
 	}
 
 	@Test
-	public void loginFailed() throws ClientProtocolException, IOException {
-		DefaultHttpClient client = new DefaultHttpClient();
-		HttpGet get = new HttpGet(deploymentUrl + "/login?username=demoiselle&password=wrongpass");
-		HttpResponse response = client.execute(get);
+	public void loginFailed() throws ClientProtocolException, IOException, URISyntaxException {
+		URIBuilder uriBuilder = new URIBuilder(deploymentUrl + "/login");
+		uriBuilder.setParameter("username", "invalid");
+		uriBuilder.setParameter("password", "invalid");
+
+		HttpGet get = new HttpGet(uriBuilder.build());
+		HttpResponse response = HttpClientBuilder.create().build().execute(get);
 
 		int status = response.getStatusLine().getStatusCode();
-		assertEquals(HttpStatus.SC_FORBIDDEN, status);
+		assertEquals(SC_FORBIDDEN, status);
 	}
 }
