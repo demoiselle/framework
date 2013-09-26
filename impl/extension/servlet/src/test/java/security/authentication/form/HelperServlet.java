28d7743f92be5505eb195d1dbbaf161ad390026f
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2013-09-26 10:30:19 -0300

    IN PROGRESS - issue FWK-119: Testes da extensão Servlet https://demoiselle.atlassian.net/browse/FWK-119

--- impl/extension/servlet/src/test/java/security/authentication/form/HelperServlet.java
+++ impl/extension/servlet/src/test/java/security/authentication/form/HelperServlet.java
@@ -1,17 +1,22 @@
 package security.authentication.form;
 
+import static org.apache.http.HttpStatus.SC_EXPECTATION_FAILED;
 import static org.apache.http.HttpStatus.SC_FORBIDDEN;
+import static org.apache.http.HttpStatus.SC_NOT_FOUND;
 import static org.apache.http.HttpStatus.SC_OK;
 
 import java.io.IOException;
+import java.security.InvalidParameterException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import br.gov.frameworkdemoiselle.security.AuthenticationException;
 import br.gov.frameworkdemoiselle.security.Credentials;
+import br.gov.frameworkdemoiselle.security.InvalidCredentialsException;
 import br.gov.frameworkdemoiselle.security.SecurityContext;
 import br.gov.frameworkdemoiselle.util.Beans;
 
@@ -21,19 +26,63 @@ public class HelperServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String result = request.getHeader("Authorization");
-		result = (result == null ? request.getHeader("authorization") : result);
+		String action = getAction(request);
 
+		if ("login".equals(action)) {
+			login(request, response);
+		} else if ("logout".equals(action)) {
+			logout(request, response);
+		} else {
+			response.setStatus(SC_NOT_FOUND);
+		}
+	}
+
+	private void login(HttpServletRequest request, HttpServletResponse response) {
+		loadCredentials(request);
+		SecurityContext securityContext = Beans.getReference(SecurityContext.class);
+
+		try {
+			securityContext.login();
+
+			if (securityContext.isLoggedIn()) {
+				response.setStatus(SC_OK);
+			} else {
+				response.setStatus(SC_FORBIDDEN);
+			}
+
+		} catch (InvalidCredentialsException e) {
+			response.setStatus(SC_FORBIDDEN);
+		}
+	}
+
+	private void logout(HttpServletRequest request, HttpServletResponse response) {
+		loadCredentials(request);
+		SecurityContext securityContext = Beans.getReference(SecurityContext.class);
+
+		securityContext.login();
+		securityContext.logout();
+
+		if (!securityContext.isLoggedIn()) {
+			response.setStatus(SC_OK);
+		} else {
+			response.setStatus(SC_EXPECTATION_FAILED);
+		}
+	}
+
+	private void loadCredentials(HttpServletRequest request) {
 		Credentials credentials = Beans.getReference(Credentials.class);
 		credentials.setUsername(request.getParameter("username"));
 		credentials.setPassword(request.getParameter("password"));
+	}
 
-		try {
-			Beans.getReference(SecurityContext.class).login();
-			response.setStatus(SC_OK);
+	private String getAction(HttpServletRequest request) {
+		Pattern pattern = Pattern.compile("^.+/(.+)$");
+		Matcher matcher = pattern.matcher(request.getRequestURI());
 
-		} catch (AuthenticationException e) {
-			response.setStatus(SC_FORBIDDEN);
+		if (matcher.matches()) {
+			return matcher.group(1).toLowerCase();
+		} else {
+			throw new InvalidParameterException("Está faltando o parâmetro de ação na URL");
 		}
 	}
 }
