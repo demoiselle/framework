0c933981bd580a0ba11d5742fce463361968221e
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2014-07-23 14:44:08 -0300

    Erro HTTP 500 não expõe o stack trace do servidor.

--- impl/extension/rest/src/main/java/br/gov/frameworkdemoiselle/internal/implementation/DefaultExceptionMapper.java
+++ impl/extension/rest/src/main/java/br/gov/frameworkdemoiselle/internal/implementation/DefaultExceptionMapper.java
@@ -1,31 +1,29 @@
-//package br.gov.frameworkdemoiselle.internal.implementation;
-//
-//import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
-//
-//import java.util.ResourceBundle;
-//
-//import javax.ws.rs.core.Response;
-//import javax.ws.rs.ext.ExceptionMapper;
-//import javax.ws.rs.ext.Provider;
-//
-//import org.slf4j.Logger;
-//
-//import br.gov.frameworkdemoiselle.util.Beans;
-//import br.gov.frameworkdemoiselle.util.NamedQualifier;
-//
-//@Provider
-//public class DefaultExceptionMapper implements ExceptionMapper<Throwable> {
-//
-//	@Override
-//	public Response toResponse(Throwable exception) {
-//		ResourceBundle bundle = Beans.getReference(ResourceBundle.class, new NamedQualifier("demoiselle-rest-bundle"));
-//		Logger logger = Beans.getReference(Logger.class);
-//
-//		logger.error(exception.getMessage(), exception);
-//
-//		// throw new DemoiselleException(cause);
-//
-//		String message = bundle.getString("internal.server.error");
-//		return Response.status(INTERNAL_SERVER_ERROR).entity(message).build();
-//	}
-//}
+package br.gov.frameworkdemoiselle.internal.implementation;
+
+import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
+
+import java.util.ResourceBundle;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.slf4j.Logger;
+
+import br.gov.frameworkdemoiselle.util.Beans;
+import br.gov.frameworkdemoiselle.util.NameQualifier;
+
+@Provider
+public class DefaultExceptionMapper implements ExceptionMapper<Throwable> {
+
+	@Override
+	public Response toResponse(Throwable exception) {
+		ResourceBundle bundle = Beans.getReference(ResourceBundle.class, new NameQualifier("demoiselle-rest-bundle"));
+		Logger logger = Beans.getReference(Logger.class);
+
+		logger.error(exception.getMessage(), exception);
+
+		String message = bundle.getString("internal.server.error");
+		return Response.status(INTERNAL_SERVER_ERROR).entity(message).build();
+	}
+}
