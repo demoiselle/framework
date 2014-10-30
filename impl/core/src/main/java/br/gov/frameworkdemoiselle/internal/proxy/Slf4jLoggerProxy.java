04970ec948d41c642b9605b825bbd3607bc9adc2
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2014-10-30 09:08:10 -0200

    FWK-201: Depreciação da injeção de SLF4J Logger
    Task-Url: https://demoiselle.atlassian.net/browse/FWK-201

--- impl/core/src/main/java/br/gov/frameworkdemoiselle/internal/proxy/Slf4jLoggerProxy.java
+++ impl/core/src/main/java/br/gov/frameworkdemoiselle/internal/proxy/Slf4jLoggerProxy.java
@@ -54,6 +54,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Marker;
 
+@Deprecated
 public class Slf4jLoggerProxy implements Logger, Serializable {
 
 	private static final long serialVersionUID = 1L;
