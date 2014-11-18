c925202c01b1506606f5753c239bfa2768930543
Danilo Costa Viana <dcviana@gmail.com>
2014-11-18 13:28:36 -0300

    Melhorada documentação

--- impl/extension/servlet/src/main/java/br/gov/frameworkdemoiselle/lifecycle/AfterSessionCreated.java
+++ impl/extension/servlet/src/main/java/br/gov/frameworkdemoiselle/lifecycle/AfterSessionCreated.java
@@ -1,13 +1,17 @@
 package br.gov.frameworkdemoiselle.lifecycle;
 
 /**
- * This interface represents an event fired before a new HTTP session is created.
+ * This interface represents an event fired after a new HTTP session is created.
  * 
  * @author serpro
  *
  */
 public interface AfterSessionCreated {
 	
+	/**
+	 * 
+	 * @return The ID of the recently created session
+	 */
 	public String getSessionId();
 
 }
--- impl/extension/servlet/src/main/java/br/gov/frameworkdemoiselle/lifecycle/BeforeSessionDestroyed.java
+++ impl/extension/servlet/src/main/java/br/gov/frameworkdemoiselle/lifecycle/BeforeSessionDestroyed.java
@@ -1,13 +1,17 @@
 package br.gov.frameworkdemoiselle.lifecycle;
 
 /**
- * This interface represents an event fired after a new HTTP session is destroyed.
+ * This interface represents an event fired before an HTTP session is destroyed.
  * 
  * @author serpro
  *
  */
 public interface BeforeSessionDestroyed {
 	
+	/**
+	 * 
+	 * @return The session ID of the session about to be destroyed
+	 */
 	public String getSessionId();
 
 }
