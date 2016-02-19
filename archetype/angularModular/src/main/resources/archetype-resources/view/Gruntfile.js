d87bfee051d000f5bf04c39aa188407d5f3dcdef
Paulo Gladson Ximenes Pinheiro <paulo.pinheiro@serpro.gov.br>
2016-02-19 15:04:39 -0200

    Correção da pasta de deploy do angular

--- archetype/angularModular/src/main/resources/archetype-resources/view/Gruntfile.js
+++ archetype/angularModular/src/main/resources/archetype-resources/view/Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function (grunt) {
     // Configurable paths for the application
     var appConfig = {
         app: require('./bower.json').appPath || 'app',
-        dist: 'src/main/webapp'
+        dist: 'dist'
     };
 
     // Define the configuration for all the tasks
