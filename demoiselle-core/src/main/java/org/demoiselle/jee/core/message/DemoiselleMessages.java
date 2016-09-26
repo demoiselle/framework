fef8a8d2571bc0ecc29c5ed91be635aa38d315b8
Julian Cesar dos Santos <julian.santos@serpro.gov.br>
2016-09-26 17:48:33 -0300

    Persistence Bootstrap

--- demoiselle-core/src/main/java/org/demoiselle/jee/core/message/DemoiselleMessages.java
+++ demoiselle-core/src/main/java/org/demoiselle/jee/core/message/DemoiselleMessages.java
@@ -4,7 +4,7 @@
  * License: GNU Lesser General Public License (LGPL), version 3 or later.
  * See the lgpl.txt file in the root directory or <https://www.gnu.org/licenses/lgpl.html>.
  */
-package org.demoiselle.jee.core.messages;
+package org.demoiselle.jee.core.message;
 
 import org.apache.deltaspike.core.api.message.MessageBundle;
 import org.apache.deltaspike.core.api.message.MessageTemplate;
---
+++ demoiselle-persistence-jpa/src/main/java/org/demoiselle/jee/persistence/jpa/bootstrap/PersistenceBootstrap.java
@@ -0,0 +1,32 @@
+package org.demoiselle.jee.persistence.jpa.bootstrap;
+
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.spi.AnnotatedType;
+import javax.enterprise.inject.spi.ProcessAnnotatedType;
+import javax.persistence.Entity;
+
+/**
+ * 
+ * Este processo evita que o CDI faça a gestão da entidade no lugar do EntityManager. Garantindo que o EntityManager faça a gestão.
+ * 
+ * http://www.cdi-spec.org/faq/
+ * Why is @Vetoed a best practice for persistent (JPA) entities?
+ * 
+ * @author SERPRO
+ *
+ */
+public class PersistenceBootstrap implements javax.enterprise.inject.spi.Extension {
+
+	/**
+	 * Adiciona Vetoed nas entidades
+	 * @param pat
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public void processAnnotatedType(@Observes final ProcessAnnotatedType pat) {
+		final AnnotatedType annotatedType = pat.getAnnotatedType();
+		if (annotatedType.getJavaClass().isAnnotationPresent(Entity.class)) {
+			pat.veto();
+		}
+	}
+
+}
