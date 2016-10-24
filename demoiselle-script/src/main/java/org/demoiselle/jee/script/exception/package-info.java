ccb17191f7a48ee8276a67be7bfa699f7e1a95e2
03066042995 <cassiomaes@gmail.com>
2016-10-24 11:35:18 -0200

    Adequações...

--- demoiselle-script/src/main/java/org/demoiselle/jee/script/exception/package-info.java
+++
@@ -1,7 +0,0 @@
-/*
- * Demoiselle Framework
- *
- * License: GNU Lesser General Public License (LGPL), version 3 or later.
- * See the lgpl.txt file in the root directory or <https://www.gnu.org/licenses/lgpl.html>.
- */
-package org.demoiselle.jee.script.exception;
