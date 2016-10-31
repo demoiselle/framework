8250f883023d0a701a0a52477b6358ae0794be9c
PauloGladson <paulo.pinheiro@serpro.gov.br>
2016-10-31 09:23:41 -0200

    #42 Javadocs

--- demoiselle-security-jwt/src/main/java/org/demoiselle/jee/security/jwt/impl/DemoiselleSecurityJWTConfig.java
+++ demoiselle-security-jwt/src/main/java/org/demoiselle/jee/security/jwt/impl/DemoiselleSecurityJWTConfig.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Demoiselle Framework
+ *
+ * License: GNU Lesser General Public License (LGPL), version 3 or later.
+ * See the lgpl.txt file in the root directory or <https://www.gnu.org/licenses/lgpl.html>.
  */
 package org.demoiselle.jee.security.jwt.impl;
 
@@ -9,6 +10,10 @@ import java.io.Serializable;
 import org.demoiselle.jee.configuration.annotation.Configuration;
 import org.demoiselle.jee.core.annotation.Name;
 
+/**
+ *
+ * @author 70744416353
+ */
 @Configuration(prefix = "demoiselle.security.jwt")
 public class DemoiselleSecurityJWTConfig implements Serializable {
 
@@ -32,50 +37,98 @@ public class DemoiselleSecurityJWTConfig implements Serializable {
     @Name("audience")
     private String destinatario;
 
+    /**
+     *
+     * @return
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * Type server ou slave
+     * @param type
+     */
     public void setType(String type) {
         this.type = type;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getPrivateKey() {
         return privateKey;
     }
 
+    /**
+     *
+     * @param privateKey
+     */
     public void setPrivateKey(String privateKey) {
         this.privateKey = privateKey;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getPublicKey() {
         return publicKey;
     }
 
+    /**
+     *
+     * @param publicKey
+     */
     public void setPublicKey(String publicKey) {
         this.publicKey = publicKey;
     }
 
+    /**
+     *
+     * @return
+     */
     public Float getTempo() {
         return tempo;
     }
 
+    /**
+     *
+     * @param tempo
+     */
     public void setTempo(Float tempo) {
         this.tempo = tempo;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getRemetente() {
         return remetente;
     }
 
+    /**
+     *
+     * @param remetente
+     */
     public void setRemetente(String remetente) {
         this.remetente = remetente;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getDestinatario() {
         return destinatario;
     }
 
+    /**
+     *
+     * @param destinatario
+     */
     public void setDestinatario(String destinatario) {
         this.destinatario = destinatario;
     }
