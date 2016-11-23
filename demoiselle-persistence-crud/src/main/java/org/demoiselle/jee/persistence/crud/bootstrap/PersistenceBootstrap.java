f1a85e17a7e2440ab65ef4335aa288473adb5c2b
PauloGladson <paulo.pinheiro@serpro.gov.br>
2016-11-23 15:59:25 -0200

    Demoiselle-crud versão para testes

---
+++ demoiselle-persistence-crud/src/main/java/org/demoiselle/jee/persistence/crud/bootstrap/PersistenceBootstrap.java
@@ -0,0 +1,39 @@
+/*
+ * Demoiselle Framework
+ *
+ * License: GNU Lesser General Public License (LGPL), version 3 or later.
+ * See the lgpl.txt file in the root directory or <https://www.gnu.org/licenses/lgpl.html>.
+ */
+package org.demoiselle.jee.persistence.crud.bootstrap;
+
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.spi.AnnotatedType;
+import javax.enterprise.inject.spi.ProcessAnnotatedType;
+import javax.persistence.Entity;
+
+/**
+ *
+ * Adding the @Vetoed annotation to all persistent entities is considered a best
+ * practice in most cases. The purpose of this annotation is to prevent the
+ * BeanManager from managing an entity as a CDI Bean.
+ *
+ * http://www.cdi-spec.org/faq/ Why is @Vetoed a best practice for persistent
+ * (JPA) entities?
+ *
+ */
+public class PersistenceBootstrap implements javax.enterprise.inject.spi.Extension {
+
+    /**
+     * Add Vetoed for all entities of application
+     *
+     * @param pat
+     */
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public void processAnnotatedType(@Observes final ProcessAnnotatedType pat) {
+        final AnnotatedType annotatedType = pat.getAnnotatedType();
+        if (annotatedType.getJavaClass().isAnnotationPresent(Entity.class)) {
+            pat.veto();
+        }
+    }
+
+}
