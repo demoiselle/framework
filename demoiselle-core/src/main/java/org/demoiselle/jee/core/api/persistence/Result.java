04985edcc3b9017849530d29c31b3bdfd4bfc3ce
Clovis Lemes Ferreira Junior <clovis.ferreira@serpro.gov.br>
2016-12-08 08:14:28 -0200

    #53 Extract ResultSet from 'crud' to 'core'

--- demoiselle-core/src/main/java/org/demoiselle/jee/core/api/persistence/Result.java
+++ demoiselle-core/src/main/java/org/demoiselle/jee/core/api/persistence/Result.java
@@ -1,32 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.demoiselle.jee.core.api.persistence;
 
 import java.util.List;
 
-/**
- *
- * @author 70744416353
- */
 public interface Result {
 
-    public int getInit();
-
-    public void setInit(int init);
-
-    public int getQtde();
-
-    public void setQtde(int qtde);
-
-    public long getTotal();
-
-    public void setTotal(long total);
-
-    public List getContent();
-
-    public void setContent(List content);
+	public Integer getOffset();
+	public void setOffset(Integer offset);
+	
+	public Integer getLimit();
+	public void setLimit(Integer limit);
+	
+	public Long getCount();
+	public void setCount(Long count);
+	
+	public List<?> getContent();
+	public void setContent(List<?> content);
 
 }
--- demoiselle-core/src/main/java/org/demoiselle/jee/core/pagination/ResultSet.java
+++ demoiselle-core/src/main/java/org/demoiselle/jee/core/pagination/ResultSet.java
@@ -1,1 +1,64 @@
+package org.demoiselle.jee.core.pagination;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+
+import org.demoiselle.jee.core.api.persistence.Result;
+
+@RequestScoped
+public class ResultSet implements Result{
+	
+	private Integer offset = new Integer(0);
+	private Integer limit = new Integer(0);
+	private Long count = new Long(0);
+	private List<?> content = new ArrayList<>();
+
+	@Override
+	public Integer getOffset() {
+		return this.offset;
+	}
+
+	@Override
+	public void setOffset(Integer offset) {
+		this.offset = offset;
+	}
+
+	@Override
+	public Integer getLimit() {
+		return this.limit;
+	}
+
+	@Override
+	public void setLimit(Integer limit) {
+		this.limit = limit;
+	}
+
+	@Override
+	public Long getCount() {
+		return this.count;
+	}
+
+	@Override
+	public void setCount(Long count) {
+		this.count = count;
+	}
+
+	@Override
+	public List<?> getContent() {
+		return this.content;
+	}
+
+	@Override
+	public void setContent(List<?> content) {
+		this.content = (List<?>) content;
+	}
+
+	@Override
+	public String toString() {
+		return "ResultSet [offset=" + offset + ", limit=" + limit + ", count=" + count + "]";
+	}
+	
+
+}
