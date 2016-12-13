abc90aaf038b71c54606650c3471651fd0166f8a
03066042995 <cassiomaes@gmail.com>
2016-12-13 13:07:13 -0200

    Ajustes...

---
+++ demoiselle-script/src/main/java/org/demoiselle/jee/script/message/DemoiselleScriptMessages.java
@@ -0,0 +1,31 @@
+/*
+ * Demoiselle Framework
+ *
+ * License: GNU Lesser General Public License (LGPL), version 3 or later.
+ * See the lgpl.txt file in the root directory or <https://www.gnu.org/licenses/lgpl.html>.
+ */
+package org.demoiselle.jee.script.message;
+
+import org.apache.deltaspike.core.api.message.MessageBundle;
+import org.apache.deltaspike.core.api.message.MessageTemplate;
+
+/**
+ *
+ * @author SERPRO
+ */
+@MessageBundle
+public interface DemoiselleScriptMessages {
+					   
+	@MessageTemplate("{error-engine-cannot-load}")
+    String cannotLoadEngine();
+
+    @MessageTemplate("{error-engine-not-loaded}")
+    String engineNotLoaded();
+        
+    @MessageTemplate("{error-engine-not-compilable}")
+    String engineNotCompilable();
+    
+    @MessageTemplate("{error-script-not-loaded}")
+    String scriptNotLoaded();
+
+}
