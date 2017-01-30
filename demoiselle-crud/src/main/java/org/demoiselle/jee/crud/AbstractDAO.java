4d52b37b8096ce4888e18d5b91f1028d8f004e1c
PauloGladson <paulo.pinheiro@serpro.gov.br>
2017-01-30 16:16:45 -0200

    Ajustes no configuration, security e crud

--- demoiselle-crud/src/main/java/org/demoiselle/jee/crud/AbstractDAO.java
+++ demoiselle-crud/src/main/java/org/demoiselle/jee/crud/AbstractDAO.java
@@ -65,22 +65,22 @@ public abstract class AbstractDAO<T, I> implements Crud<T, I> {
     @Override
     public T mergeHalf(I id, T entity) {
         try {
-            StringBuilder sb = new StringBuilder();
-            sb.append("UPDATE ");
-            sb.append(entity.getClass().getCanonicalName());
-            sb.append(" SET ");
-            for (Method method : entityClass.getDeclaredMethods()) {
-                Object obj = method.invoke(entityClass);
-                if (obj != null) {
-                    sb.append(method.getName()).append(" = ").append(obj);
-                }
-            }
-            sb.append(" WHERE ")
-                    .append(CrudUtilHelper.getMethodAnnotatedWithID(entityClass))
-                    .append(" = ")
-                    .append(id);
-            System.err.println(sb.toString());
-            //getEntityManager().createQuery(sb.toString()).executeUpdate();
+//            StringBuilder sb = new StringBuilder();
+//            sb.append("UPDATE ");
+//            sb.append(entity.getClass().getCanonicalName());
+//            sb.append(" SET ");
+//            for (Method method : entityClass.getDeclaredMethods()) {
+//                Object obj = method.invoke(entityClass);
+//                if (obj != null) {
+//                    sb.append(method.getName()).append(" = ").append(obj);
+//                }
+//            }
+//            sb.append(" WHERE ")
+//                    .append(CrudUtilHelper.getMethodAnnotatedWithID(entityClass))
+//                    .append(" = ")
+//                    .append(id);
+//            System.err.println(sb.toString());
+//            //getEntityManager().createQuery(sb.toString()).executeUpdate();
             return entity;
         } catch (Exception e) {
             // TODO: Severe? Pode cair aqui somente por ter violação de Unique
