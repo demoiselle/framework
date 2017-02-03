3efb135de38a7c53ba83550317ace8499200390b
Paulo Gladson <paulopinheiro777@gmail.com>
2017-02-03 19:14:52 -0200

    Ajuste para deploy

--- demoiselle-core/src/main/java/org/demoiselle/jee/core/api/security/TokenType.java
+++ demoiselle-core/src/main/java/org/demoiselle/jee/core/api/security/TokenType.java
@@ -1,6 +1,13 @@
+/*
+ * Demoiselle Framework
+ *
+ * License: GNU Lesser General Public License (LGPL), version 3 or later.
+ * See the lgpl.txt file in the root directory or <https://www.gnu.org/licenses/lgpl.html>.
+ */
 package org.demoiselle.jee.core.api.security;
 
 /**
+ * Types of token
  *
  * @author SERPRO
  */
--- demoiselle-crud/src/main/java/org/demoiselle/jee/crud/field/FieldHelperMessage.java
+++ demoiselle-crud/src/main/java/org/demoiselle/jee/crud/field/FieldHelperMessage.java
@@ -1,8 +1,8 @@
 /*
-  * Demoiselle Framework
-  *
-  * License: GNU Lesser General Public License (LGPL), version 3 or later.
-  * See the lgpl.txt file in the root directory or <https://www.gnu.org/licenses/lgpl.html>.
+ * Demoiselle Framework
+ *
+ * License: GNU Lesser General Public License (LGPL), version 3 or later.
+ * See the lgpl.txt file in the root directory or <https://www.gnu.org/licenses/lgpl.html>.
  */
 package org.demoiselle.jee.crud.field;
 
