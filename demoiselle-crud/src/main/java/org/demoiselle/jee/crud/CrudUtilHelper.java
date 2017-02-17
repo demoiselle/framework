05b2a9335c4f125e3b782d1589721e6854c86594
PauloGladson <paulo.pinheiro@serpro.gov.br>
2017-02-17 14:52:30 -0200

    publicação

--- demoiselle-crud/src/main/java/org/demoiselle/jee/crud/CrudUtilHelper.java
+++ demoiselle-crud/src/main/java/org/demoiselle/jee/crud/CrudUtilHelper.java
@@ -19,17 +19,18 @@ import java.util.regex.Pattern;
 import javax.ws.rs.container.ResourceInfo;
 
 /**
- * Class used to support CRUD feature. 
- * 
+ * Class used to support CRUD feature.
+ *
  * @author SERPRO
  */
 public class CrudUtilHelper {
 
     /**
-     * Given a Class that extends {@link AbstractREST} this method will return the target Class used on {@literal AbstractREST<TargetClass, I>}
-     * 
+     * Given a Class that extends {@link AbstractREST} this method will return
+     * the target Class used on {@literal AbstractREST<TargetClass, I>}
+     *
      * @param targetClass Target class
-     * 
+     *
      * @return Class used on {@literal AbstractREST<TargetClass, I>}
      */
     public static Class<?> getTargetClass(Class<?> targetClass) {
@@ -42,34 +43,39 @@ public class CrudUtilHelper {
 
     /**
      * Check if the 'field' parameter exists on 'targetClass'
-     * 
+     *
      * @param targetClass The class
      * @param field Field to checked
-     * 
-     * @throws IllegalArgumentException When the 'field' doesn't exists on 'targetClass'
+     *
+     * @throws IllegalArgumentException When the 'field' doesn't exists on
+     * 'targetClass'
      */
     public static void checkIfExistField(Class<?> targetClass, String field) {
         if (targetClass != null) {
-            try {
-                targetClass.getDeclaredField(field);
-            } 
-            catch (NoSuchFieldException e) {
-                throw new IllegalArgumentException(e);
-            }
+            do {
+                Field[] fields = targetClass.getDeclaredFields();
+                for (Field f : fields) {
+                    if (f.getName().equalsIgnoreCase(field)) {
+                        return;
+                    }
+                }
+                targetClass = targetClass.getSuperclass();
+            } while (targetClass != null);
+            throw new IllegalArgumentException();
         }
     }
-    
+
     /**
-     * Given a string like 'field1,field2(subField1,subField2)' this method will 
+     * Given a string like 'field1,field2(subField1,subField2)' this method will
      * return a List with [field1, field2(subField1,subField2)] values
-     * 
+     *
      * @param fields String with field to be extracted
-     * 
+     *
      * @return List Parsed values
      */
     public static List<String> extractFields(String fields) {
         List<String> results = new LinkedList<>();
-        
+
         int qtyOpenParentheses = 0;
         int qtyCloseParentheses = 0;
 
@@ -79,216 +85,209 @@ public class CrudUtilHelper {
 
         char fieldsArray[] = fields.toCharArray();
 
-        for(int i = 0 ; i < fieldsArray.length ; i++){
+        for (int i = 0; i < fieldsArray.length; i++) {
 
             char letter = fieldsArray[i];
-                
+
             // Find util the next ',' character
-            if( letter == ',' ) {
+            if (letter == ',') {
                 lastComma = i;
-                
+
                 subField = fields.substring(lastPosition, lastComma);
-                
+
                 // If the subset doesn't exists open parentheses add the field
-                if(!subField.contains("(")){
+                if (!subField.contains("(")) {
                     results.add(subField);
                     lastPosition = lastComma + 1;
-                }
-                else{
+                } else {
                     qtyCloseParentheses = 0;
                     qtyOpenParentheses = 0;
                     Boolean closeAllParentheses = Boolean.FALSE;
-                    
+
                     // Find the close parentheses
-                    for(int j = lastPosition ; j < fieldsArray.length ; j++){
+                    for (int j = lastPosition; j < fieldsArray.length; j++) {
                         char subLetter = fieldsArray[j];
-                        
-                        if(subLetter == '(') {
+
+                        if (subLetter == '(') {
                             qtyOpenParentheses++;
                         }
-                        
-                        if(subLetter == ')') {
+
+                        if (subLetter == ')') {
                             qtyCloseParentheses++;
                         }
-                        
-                        if(qtyCloseParentheses > 0 && qtyOpenParentheses > 0 
-                            && qtyCloseParentheses == qtyOpenParentheses) { 
-                                                              
+
+                        if (qtyCloseParentheses > 0 && qtyOpenParentheses > 0
+                                && qtyCloseParentheses == qtyOpenParentheses) {
+
                             subField = fields.substring(lastPosition, ++j);
                             results.add(subField);
                             closeAllParentheses = Boolean.TRUE;
-                            
-                            lastPosition = j+1;
-                            i = j+1;          
+
+                            lastPosition = j + 1;
+                            i = j + 1;
 
                             break;
                         }
-                        
+
                     }
-                    
-                    if(closeAllParentheses == Boolean.FALSE){
+
+                    if (closeAllParentheses == Boolean.FALSE) {
                         throw new IllegalArgumentException();
                     }
-                    
+
                 }
             }
 
         }
-        
-        if(lastPosition < fieldsArray.length){
+
+        if (lastPosition < fieldsArray.length) {
             subField = fields.substring(lastPosition, fieldsArray.length);
             results.add(subField);
         }
-        
+
         return results;
     }
-    
+
     /**
      * Fill the {@link TreeNodeField} object based on parameters.
-     * 
-     * @param tnf TreeNodeField actual node 
+     *
+     * @param tnf TreeNodeField actual node
      * @param field String of field
      * @param value List with values
      */
     public static void fillLeafTreeNodeField(TreeNodeField<String, Set<String>> tnf, String field, Set<String> value) {
-        
+
         String actualField = field;
-        
-        if(hasSubField(actualField)){
+
+        if (hasSubField(actualField)) {
             String masterField = actualField.substring(0, actualField.indexOf("("));
-            
+
             TreeNodeField<String, Set<String>> masterTNF = getTreeNodeField(masterField, tnf);
-           
+
             actualField = actualField.substring(actualField.indexOf("(") + 1, actualField.length() - 1);
             List<String> subFields = extractFields(actualField);
-            for(String actualSubField : subFields){
-            
-                if(!hasSubField(actualSubField)){
+            for (String actualSubField : subFields) {
+
+                if (!hasSubField(actualSubField)) {
                     masterTNF.addChild(actualSubField, value);
-                }
-                else{
+                } else {
                     fillLeafTreeNodeField(masterTNF, actualSubField, value);
                 }
             }
-        }
-        else{
+        } else {
             tnf.addChild(actualField, value);
         }
-        
+
     }
-    
+
     private static TreeNodeField<String, Set<String>> getTreeNodeField(String masterField, TreeNodeField<String, Set<String>> tnf) {
         TreeNodeField<String, Set<String>> tnfFinded = tnf.getChildren().stream()
-                                                                .filter( child -> child.getKey().equalsIgnoreCase(masterField))
-                                                                .findAny()
-                                                                .orElse(null);
-        
+                .filter(child -> child.getKey().equalsIgnoreCase(masterField))
+                .findAny()
+                .orElse(null);
+
         return tnfFinded == null ? tnf.addChild(masterField, null) : tnfFinded;
     }
 
     /**
-     * Validate the fields based on {@link Search#fields()} or if field exists on targetClass
-     * 
+     * Validate the fields based on {@link Search#fields()} or if field exists
+     * on targetClass
+     *
      * @param tnf TreeNodeField filled
      * @param resourceInfo ResourceInfo
      * @param crudMessage CrudMessage
      */
     public static void validateFields(TreeNodeField<String, Set<String>> tnf, ResourceInfo resourceInfo, CrudMessage crudMessage) {
-        
+
         // Get fields from @Search.fields attribute
         List<String> fieldsFromAnnotation = new ArrayList<>();
         final TreeNodeField<String, Set<String>> searchFieldsTnf = new TreeNodeField<>(getTargetClass(resourceInfo.getResourceClass()).getName(), null);
-        
-        if(resourceInfo.getResourceMethod().isAnnotationPresent(Search.class)){
+
+        if (resourceInfo.getResourceMethod().isAnnotationPresent(Search.class)) {
             String fieldsAnnotation[] = resourceInfo.getResourceMethod().getAnnotation(Search.class).fields();
-            
-            if(fieldsAnnotation != null && !fieldsAnnotation[0].equals("*")){
-            
+
+            if (fieldsAnnotation != null && !fieldsAnnotation[0].equals("*")) {
+
                 fieldsFromAnnotation.addAll(Arrays.asList(fieldsAnnotation));
-                if(!fieldsFromAnnotation.isEmpty()){
+                if (!fieldsFromAnnotation.isEmpty()) {
                     // Transform fields from annotation into TreeNodeField                
-                    fieldsFromAnnotation.stream().forEach( searchField -> {
+                    fieldsFromAnnotation.stream().forEach(searchField -> {
                         fillLeafTreeNodeField(searchFieldsTnf, searchField, null);
                     });
                 }
             }
         }
-        
+
         //Validate fields
-        tnf.getChildren().stream().forEach( leaf -> {
-            
-            if(!searchFieldsTnf.getChildren().isEmpty()){
-                
-                try{
+        tnf.getChildren().stream().forEach(leaf -> {
+
+            if (!searchFieldsTnf.getChildren().isEmpty()) {
+
+                try {
                     // 1st level
-                    if(!searchFieldsTnf.containsKey(leaf.getKey())){
+                    if (!searchFieldsTnf.containsKey(leaf.getKey())) {
                         throw new IllegalArgumentException(crudMessage.fieldRequestDoesNotExistsOnSearchField(leaf.getKey()));
                     }
-                    
-                    if(!leaf.getChildren().isEmpty()){
-                        leaf.getChildren().stream().forEach( leafItem -> {
-                            
+
+                    if (!leaf.getChildren().isEmpty()) {
+                        leaf.getChildren().stream().forEach(leafItem -> {
+
                             /*
                              * Given a @Search(fields={field1,field2}) and a request like a 'fields=field1,field2(subField1)' 
                              * the request is valid because de @Search.fields specified the root type (field2)
                              *
                              */
-                            if(!searchFieldsTnf.getChildByKey(leafItem.getParent().getKey()).getChildren().isEmpty()){
+                            if (!searchFieldsTnf.getChildByKey(leafItem.getParent().getKey()).getChildren().isEmpty()) {
 
-                                searchFieldsTnf.getChildByKey(leafItem.getParent().getKey()).getChildren().stream().forEach( sfTnf -> {
-                                    if(!sfTnf.getParent().containsKey(leafItem.getKey())){
+                                searchFieldsTnf.getChildByKey(leafItem.getParent().getKey()).getChildren().stream().forEach(sfTnf -> {
+                                    if (!sfTnf.getParent().containsKey(leafItem.getKey())) {
                                         throw new IllegalArgumentException(crudMessage.fieldRequestDoesNotExistsOnSearchField(leafItem.getParent().getKey() + "(" + leafItem.getKey() + ")"));
                                     }
                                 });
-                                
+
                             }
                         });
                     }
-                }
-                catch(IllegalArgumentException e){
+                } catch (IllegalArgumentException e) {
                     throw e;
                 }
             }
-            
+
             Class<?> targetClass = getTargetClass(resourceInfo.getResourceClass());
-            
-            if(!leaf.getChildren().isEmpty()){
+
+            if (!leaf.getChildren().isEmpty()) {
                 Field fieldMaster;
-                
+
                 try {
                     fieldMaster = targetClass.getDeclaredField(leaf.getKey());
-                }
-                catch (SecurityException | NoSuchFieldException e) {
+                } catch (SecurityException | NoSuchFieldException e) {
                     throw new IllegalArgumentException(crudMessage.fieldRequestDoesNotExistsOnObject(leaf.getKey(), targetClass.getName()));
                 }
-                
+
                 Class<?> fieldClazz = fieldMaster.getType();
-                                        
-                leaf.getChildren().forEach( (subLeaf) -> {
-                    try{
+
+                leaf.getChildren().forEach((subLeaf) -> {
+                    try {
                         CrudUtilHelper.checkIfExistField(fieldClazz, subLeaf.getKey());
-                    }
-                    catch(IllegalArgumentException e){
+                    } catch (IllegalArgumentException e) {
                         throw new IllegalArgumentException(crudMessage.fieldRequestDoesNotExistsOnObject(subLeaf.getKey(), fieldClazz.getName()));
                     }
                 });
-            }
-            else{
-                try{
+            } else {
+                try {
                     CrudUtilHelper.checkIfExistField(getTargetClass(resourceInfo.getResourceClass()), leaf.getKey());
-                }
-                catch(IllegalArgumentException e){
+                } catch (IllegalArgumentException e) {
                     throw new IllegalArgumentException(crudMessage.fieldRequestDoesNotExistsOnObject(leaf.getKey(), getTargetClass(resourceInfo.getResourceClass()).getName()));
                 }
             }
         });
-        
+
     }
-    
-    private static Boolean hasSubField(String field){
+
+    private static Boolean hasSubField(String field) {
         Pattern patternLevels = Pattern.compile("\\([^)]*\\)*");
         Matcher matcher = patternLevels.matcher(field);
-        
+
         return matcher.find();
     }
 
@@ -296,6 +295,5 @@ public class CrudUtilHelper {
     public static String getMethodAnnotatedWithID(Class<?> targetClass) {
         return "id";
     }
-
 
 }
