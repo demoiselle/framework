c4113ad6abcc4e60b1a371bba73b2880e53b9bf4
PauloGladson <paulo.pinheiro@serpro.gov.br>
2017-03-14 14:32:05 -0300

    #68 Permitir ordenação quando usar * no @Search

--- demoiselle-crud/src/main/java/org/demoiselle/jee/crud/sort/SortHelper.java
+++ demoiselle-crud/src/main/java/org/demoiselle/jee/crud/sort/SortHelper.java
@@ -26,41 +26,44 @@ import org.demoiselle.jee.crud.ReservedKeyWords;
 import org.demoiselle.jee.crud.Search;
 
 /**
- * Class responsible for managing the 'sort' parameter comes from Url Query String.
- * 
+ * Class responsible for managing the 'sort' parameter comes from Url Query
+ * String.
+ *
  * Ex:
- * 
+ *
  * Given a request
  * <pre>
  * GET {@literal http://localhost:8080/api/users?sort=field1,field2&desc=field2}
  * </pre>
- * 
- * This class will processing the request above and parse the 'sort=...' and 'desc=...' parameters to 
- * list of {@link SortModel} objects.
- * 
- * This list will be use on {@link AbstractDAO} class to execute the sort on database.
- * 
+ *
+ * This class will processing the request above and parse the 'sort=...' and
+ * 'desc=...' parameters to list of {@link SortModel} objects.
+ *
+ * This list will be use on {@link AbstractDAO} class to execute the sort on
+ * database.
+ *
  * @author SERPRO
  */
 @RequestScoped
 public class SortHelper {
-    
+
     private ResourceInfo resourceInfo;
-    
+
     private UriInfo uriInfo;
-    
+
     @Inject
     private DemoiselleRequestContext drc;
-    
+
     @Inject
     private SortHelperMessage sortHelperMessage;
-    
+
     @Inject
     private CrudMessage crudMessage;
-    
-    public SortHelper(){}
-    
-    public SortHelper(ResourceInfo resourceInfo, UriInfo uriInfo, DemoiselleRequestContext drc, SortHelperMessage sortHelperMessage, CrudMessage crudMessage){
+
+    public SortHelper() {
+    }
+
+    public SortHelper(ResourceInfo resourceInfo, UriInfo uriInfo, DemoiselleRequestContext drc, SortHelperMessage sortHelperMessage, CrudMessage crudMessage) {
         this.resourceInfo = resourceInfo;
         this.uriInfo = uriInfo;
         this.drc = drc;
@@ -69,86 +72,85 @@ public class SortHelper {
     }
 
     /**
-     * Open the request query string to extract values from 'sort' and 'desc' parameters and 
-     * fill the {@link DemoiselleRequestContext#setSorts(List)}
-     * 
+     * Open the request query string to extract values from 'sort' and 'desc'
+     * parameters and fill the {@link DemoiselleRequestContext#setSorts(List)}
+     *
      * @param resourceInfo ResourceInfo
      * @param uriInfo UriInfo
      */
     public void execute(ResourceInfo resourceInfo, UriInfo uriInfo) {
         this.resourceInfo = resourceInfo == null ? this.resourceInfo : resourceInfo;
         this.uriInfo = uriInfo == null ? this.uriInfo : uriInfo;
-        
+
         Set<String> descList = new LinkedHashSet<>();
         Boolean descAll = Boolean.FALSE;
-        
+
         List<String> descValues = getValuesFromQueryString(ReservedKeyWords.DEFAULT_SORT_DESC_KEY.getKey());
         List<String> sortValues = getValuesFromQueryString(ReservedKeyWords.DEFAULT_SORT_KEY.getKey());
-        
+
         // 'desc' parameter was filled and 'sort' parameter not
-        if(descValues != null && sortValues == null) {
+        if (descValues != null && sortValues == null) {
             throw new IllegalArgumentException(sortHelperMessage.descParameterWithoutSortParameter());
         }
-        
-        if(descValues != null) {
-            
-          //&desc without parameters
-            if(descValues.isEmpty()) {
+
+        if (descValues != null) {
+
+            //&desc without parameters
+            if (descValues.isEmpty()) {
                 descAll = Boolean.TRUE;
-            }
-            else{
-                for(String value : descValues){
+            } else {
+                for (String value : descValues) {
                     //&desc=a,b,c
-                    if(!value.isEmpty()){
-                        if(!sortValues.contains(value)){
-                            throw new BadRequestException(crudMessage.fieldRequestDoesNotExistsOnSearchField(value)); 
+                    if (!value.isEmpty()) {
+                        if (!sortValues.contains(value)) {
+                            throw new BadRequestException(crudMessage.fieldRequestDoesNotExistsOnSearchField(value));
                         }
                         descList.add(value);
                     }
                 }
             }
         }
-        
-        if(sortValues != null) {
-                
-            for(String field : sortValues) {
-                if(descAll == Boolean.TRUE){
+
+        if (sortValues != null) {
+
+            for (String field : sortValues) {
+                if (descAll == Boolean.TRUE) {
                     drc.getSorts().add(new SortModel(CrudSort.DESC, field));
-                }
-                else{
+                } else {
                     // Field was set to desc
-                    if(descList.contains(field)){
+                    if (descList.contains(field)) {
                         drc.getSorts().add(new SortModel(CrudSort.DESC, field));
-                    }
-                    else{
+                    } else {
                         drc.getSorts().add(new SortModel(CrudSort.ASC, field));
                     }
                 }
             }
         }
-        
+
         //Valid if fields exists on fields attribute from @Search annotation
-        if(this.resourceInfo.getResourceMethod().isAnnotationPresent(Search.class)){
+        if (this.resourceInfo.getResourceMethod().isAnnotationPresent(Search.class)) {
             Search search = this.resourceInfo.getResourceMethod().getAnnotation(Search.class);
             List<String> searchFields = Arrays.asList(search.fields());
-            drc.getSorts().stream().filter((sortModel) -> (!searchFields.contains(sortModel.getField()))).forEachOrdered((sortModel) -> {
-                throw new BadRequestException(crudMessage.fieldRequestDoesNotExistsOnSearchField(sortModel.getField()));
-            });
+            if (searchFields.get(0) != null && !searchFields.get(0).equals("*")) {
+                drc.getSorts().stream().filter((sortModel) -> (!searchFields.contains(sortModel.getField()))).forEachOrdered((sortModel) -> {
+                    throw new BadRequestException(crudMessage.fieldRequestDoesNotExistsOnSearchField(sortModel.getField()));
+                });
+            }
         }
-        
+
         // Validate if the fields are valid
-        drc.getSorts().stream().forEach( sortModel -> {
+        drc.getSorts().stream().forEach(sortModel -> {
             CrudUtilHelper.checkIfExistField(CrudUtilHelper.getTargetClass(this.resourceInfo.getResourceClass()), sortModel.getField());
         });
-        
+
     }
 
     private List<String> getValuesFromQueryString(String key) {
-        
-        for(String queryStringKey : uriInfo.getQueryParameters().keySet()){
+
+        for (String queryStringKey : uriInfo.getQueryParameters().keySet()) {
             if (key.equalsIgnoreCase(queryStringKey)) {
                 List<String> result = new LinkedList<>();
-                for(String value : uriInfo.getQueryParameters().get(queryStringKey)){
+                for (String value : uriInfo.getQueryParameters().get(queryStringKey)) {
                     result.addAll(CrudUtilHelper.extractFields(value));
                 }
                 return result;
