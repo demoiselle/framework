439a9781514cb486d4404fc25c9322a0fcc0fdbe
Vanderson Botelho da Silva <vanderson.silva@serpro.gov.br>
2017-12-07 16:19:20 -0200

    remocao de testes para geracao da build 2.5.2

--- impl/extension/jpa/src/test/java/template/MyNamedCrud.java
+++
@@ -1,9 +0,0 @@
-package template;
-
-import br.gov.frameworkdemoiselle.template.JPACrud;
-
-public class MyNamedCrud extends JPACrud<MyNamedEntity, String> {
-
-	private static final long serialVersionUID = 1L;
-
-}
